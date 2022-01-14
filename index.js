1c3255cbc83d417d5c7d8528e3f7dbf906113b15
jcpcabanada <jcpcabanada@gmail.com>
2022-01-14 10:38:32 -0800

    ttesting

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const cohort1 = process.argv[2]
-const user = process.env.USERDOMAIN
+require('dotenv').config()
+
+// const cohort1 = process.argv[2]
+// const user = process.env.USERDOMAIN
 const express = require('express')
 const app = express()
-const port = 9000
+const port = process.env.PORT || 9000
 
-if (cohort1 === 'web-49') {
-    console.log(`${cohort1} is the best forever!`)
-} else {
-    console.log(`booooh cohort ${cohort1}`)
-}
+// if (cohort1 === 'web-49') {
+//     console.log(`${cohort1} is the best forever!`)
+// } else {
+//     console.log(`booooh cohort ${cohort1}`)
+// }
 
 console.log(`the user is ${user}`)
 
